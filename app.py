c297cb1041789669bcdad7ad2f36027690cb7f2a
Angel David Castillo Castro <adcc662@gmail.com>
2022-07-07 16:20:39 -0500

    Adding files of docker

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/universities")
+def get_universities():
+    API_URL = "http://universities.hipolabs.com/search?country="
+    search = request.args.get('country')
+    r = requests.get(API_URL + search)
+    return jsonify(r.json())
+
+if __name__ == "__main__":
+    app.run(debug=True, host= "0.0.0.0", port= 5000)
+    
